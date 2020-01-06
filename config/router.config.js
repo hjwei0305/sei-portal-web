d3e6ad2e66313b6a0dd5ef845db4430e4924e7f2
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-01-06 21:36:13 +0800

    测试构建

--- config/router.config.js
+++ config/router.config.js
@@ -1,33 +1,34 @@
-const routes = [{
-    path: '/user',
-    component: '../layouts/LoginLayout',
-    routes: [
-      {
-        name: 'login',
-        path: '/user/login',
-        component: './userLogin',
-      },
-    ],
-  },
-  {
-    path: '/',
-    component: '../layouts/BasicLayout',
-    routes: [
-      {
-        path: '/',
-        redirect: '/DashBoard',
-      },
-      {
-        path: '/DashBoard',
-        name: 'DashBoard',
-        component: './DashBoard',
-      },
-      {
-        component: './DashBoard',
-        // component: './404',
-      },
-    ],
-  },
+const routes = [
+// {
+//     path: '/user',
+//     component: '../layouts/LoginLayout',
+//     routes: [
+//       {
+//         name: 'login',
+//         path: '/user/login',
+//         component: './userLogin',
+//       },
+//     ],
+//   },
+//   {
+//     path: '/',
+//     component: '../layouts/BasicLayout',
+//     routes: [
+//       {
+//         path: '/',
+//         redirect: '/DashBoard',
+//       },
+//       {
+//         path: '/DashBoard',
+//         name: 'DashBoard',
+//         component: './DashBoard',
+//       },
+//       {
+//         component: './DashBoard',
+//         // component: './404',
+//       },
+//     ],
+//   },
   {
     component: './DashBoard',
     // component: './404',
