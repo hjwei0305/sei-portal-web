b5bea1862064acff73953ba3b49bb44e04cb5f76
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-01-06 20:27:42 +0800

    修改userLogin大小写

--- config/router.config.js
+++ config/router.config.js
@@ -5,7 +5,7 @@ const routes = [{
       {
         name: 'login',
         path: '/user/login',
-        component: './UserLogin',
+        component: './userLogin',
       },
     ],
   },
---
+++ src/pages/userLogin/locales/zh-CN.js
@@ -0,0 +1,13 @@
+export default {
+  'user-login.login.userName': '用户名',
+  'user-login.login.password': '密码',
+  'user-login.login.message-invalid-credentials': '账户或密码错误',
+  'user-login.login.message-invalid-verification-code': '验证码错误',
+  'user-login.login.tab-login-credentials': '账户密码登录',
+  'user-login.login.forgot-password': '忘记密码',
+  'user-login.login.signup': '注册账户',
+  'user-login.login.login': '登录',
+  'user-login.login.loginning': '登录中...',
+  'user-login.register.register': '注册',
+  'user-login.register.get-verification-code': '获取验证码',
+};
