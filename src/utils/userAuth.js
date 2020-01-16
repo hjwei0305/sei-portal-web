663b74e3e4058f1fc4a3721ae87b8a128acc86c1
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-01-16 20:36:58 +0800

    更新seid包

--- src/utils/userAuth.js
+++ src/utils/userAuth.js
@@ -2,13 +2,13 @@
  * @Author: zp
  * @Date:   2020-01-16 10:51:41
  * @Last Modified by:   zp
- * @Last Modified time: 2020-01-16 15:36:48
+ * @Last Modified time: 2020-01-16 20:35:51
  */
 import { utils } from 'seid';
 
 const { sessionStorage } = utils.storage;
 const { CONST_GLOBAL } = utils.constants;
-const { CURRENT_USER, SESSION } = CONST_GLOBAL;
+const { CURRENT_USER, TOKEN_KEY } = CONST_GLOBAL;
 
 /** 用户信息保存到session */
 export const setCurrentUser = user => {
@@ -20,11 +20,11 @@ export const getCurrentUser = () => sessionStorage.get(CURRENT_USER);
 
 /** sid保存到session */
 export const setSessionId = sid => {
-  sessionStorage.set(SESSION, sid);
+  sessionStorage.set(TOKEN_KEY, sid);
 };
 
 /** 获取当前sid */
-export const getSessionId = () => sessionStorage.get(SESSION);
+export const getSessionId = () => sessionStorage.get(TOKEN_KEY);
 
 /** 根据键清空 */
-export const clearUserInfo = () => sessionStorage.clear([CURRENT_USER, SESSION]);
+export const clearUserInfo = () => sessionStorage.clear([CURRENT_USER, TOKEN_KEY]);
