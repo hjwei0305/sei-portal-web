97bc5b19da82356b37eda643c646df4dcba4db8b
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-01-19 10:52:20 +0800

    修改登录页面的布局

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -1,34 +1,27 @@
 import { Helmet } from 'react-helmet';
-import Link from 'umi/link';
 import React from 'react';
-import { connect } from 'dva';
-import { formatMessage } from 'umi-plugin-react/locale';
 import logo from '../assets/logo@2x.png';
 import styles from './LoginLayout.less';
 
 const LoginLayout = props => {
-  const { children, } = props;
+  const { children } = props;
 
   return (
     <>
       <Helmet>
-        <title>{'登录'}</title>
-        <meta name="description" content={'登录'} />
+        <title>登录</title>
+        <meta name="description" content="登录" />
       </Helmet>
-      <div className={styles["login-wrapper"]}>
-        <div className="header-wrapper w">
+      <div className={styles['login-wrapper']}>
+        <div className="header-wrapper">
           <div className="logo">
-            <img src={logo} alt="logo"/>
+            <img src={logo} alt="logo" />
           </div>
         </div>
         <div className="content-wrapper">
-          <div className="w">
-            <div className="login-form">
-              <div className="login-form-title">
-                SEI账号登录
-              </div>
-              {children}
-            </div>
+          <div className="login-form">
+            <div className="login-form-title">SEI账号登录</div>
+            {children}
           </div>
         </div>
       </div>
