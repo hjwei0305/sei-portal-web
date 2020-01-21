48e395c04d924dd72b620ffe0e9f7968ddb3d90c
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-01-21 09:13:42 +0800

    请求子配置的时候添加时间戳

--- src/services/microFront.js
+++ src/services/microFront.js
@@ -1,43 +1,38 @@
-import { request, } from '@/utils';
+import { request } from '@/utils';
 
-export const getSubAppConfig = () => {
+export const getSubAppConfig = () =>
   /** 获取子应用注册表 */
-  return request.get('/apps.config.json').then(res => {
-    // const reg = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\? i)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/;
-    const { success, data } = res || {};
-    let apps = [];
-    if (success) {
-      apps= data.map(item => {
-        // if (!reg.test(item.entry)) {
-        //   item.entry = window.location.origin + item.entry;
-        // }
-        return item;
-      });
-    }
+  request
+    .get(`/apps.config.json?random=${new Date().getTime()}`)
+    .then(res => {
+      // const reg = /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\? i)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/;
+      const { success, data } = res || {};
+      let apps = [];
+      if (success) {
+        apps = data.map(item => item);
+      }
 
-    return {
-      apps,
-      defer: true,
-      // 是否启用 js 沙箱，默认为 false
-      jsSandbox: false,
-      prefetch: true,
-      lifeCycles: {
-        afterMount: props => {
-          /** todo */
-        },
-      },
-    };
-  }).catch(err => {
-    return {
+      return {
+        apps,
+        defer: true,
+        // 是否启用 js 沙箱，默认为 false
+        jsSandbox: false,
+        prefetch: true,
+        // lifeCycles: {
+        //   afterMount: props => {
+        //     /** todo */
+        //   },
+        // },
+      };
+    })
+    .catch(() => ({
       apps: [],
       defer: true,
       jsSandbox: false,
       prefetch: true,
-      lifeCycles: {
-        afterMount: props => {
-          /** todo */
-        },
-      },
-    };
-  });
-}
+      // lifeCycles: {
+      //   afterMount: props => {
+      //     /** todo */
+      //   },
+      // },
+    }));
