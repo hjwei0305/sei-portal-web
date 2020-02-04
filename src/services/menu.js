2142a6ce77fb901daecfbc5c1740eafa27c4f312
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-02-04 16:54:25 +0800

    集成菜单接口

--- src/services/menu.js
+++ src/services/menu.js
@@ -2,13 +2,15 @@
  * @Author: zp
  * @Date:   2020-01-09 15:57:34
  * @Last Modified by:   zp
- * @Last Modified time: 2020-02-04 14:02:29
+ * @Last Modified time: 2020-02-04 16:54:09
  */
 import { request, CONSTANTS } from '@/utils';
 
-const { PORTALSERVICE } = CONSTANTS;
-// `${BASICSERVICE}/menu/getMenuTree`,
+// const { getCurrentUser } = userInfoOperation;
+// const { userId } = getCurrentUser() || {};
+const { BASICSERVICE } = CONSTANTS;
 export const getMenu = () =>
   request.get(
-    `http://rddgit.changhong.com:7300/mock/5dfc741736608e42d52b1d7b${PORTALSERVICE}/getMenus`,
+    // `${BASICSERVICE}/user/getUserAuthorizedMenus?userId=${userId}`,
+    `${BASICSERVICE}/menu/getMenuTree`,
   );
