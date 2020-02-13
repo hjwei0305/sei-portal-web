0af75f1ea246a7916e9342a4cc3aa8bcf9ecc7f7
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-02-13 22:35:48 +0800

    添加基于 Hooks 共享数据 约定父应用中在 src/rootExports.js 里 export 内容 子应用中通过 import { useRootExports } from 'umi'; const rootExports = useRootExports(); 取到

---
+++ rootExports.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: zp
+ * @Date:   2020-02-13 22:25:59
+ * @Last Modified by:   zp
+ * @Last Modified time: 2020-02-13 22:31:48
+ */
+export function reLogin() {
+  window.location.replace('/sei-portal-web/#/user/login');
+}
