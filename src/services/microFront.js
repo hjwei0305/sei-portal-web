c8afe43efc90b1a1d2c833a274fe60d692deedd5
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-02-15 13:18:59 +0800

    修改配置文件

--- src/services/microFront.js
+++ src/services/microFront.js
@@ -1,41 +1,52 @@
-import { request } from '@/utils';
+// import { request } from '@/utils';
 
 export const getSubAppConfig = () =>
-  /** 获取子应用注册表 */
-  request
-    .get(`/apps.config.json?random=${new Date().getTime()}`)
-    .then(res => {
-      const { success, data } = res || {};
-      let apps = [];
-      if (success) {
-        apps = data.map(item => item);
-        // apps = data.map(item => {
-        //   item.entry += `/index.html?random=${new Date().getTime()}`;
-        //   return item;
-        // });
-      }
+  Promise.resolve({
+    apps: [
+      { name: 'sei-basic-web', entry: '/sei-basic-web', base: '/sei-basic-web' },
+      { name: 'sei-notify-web', entry: '/sei-notify-web', base: '/sei-notify-web' },
+      { name: 'sei-serial-web', entry: '/sei-serial-web', base: '/sei-serial-web' },
+      { name: 'sei-task-web', entry: '/sei-task-web', base: '/sei-task-web' },
+    ],
+    defer: true,
+    jsSandbox: false,
+    prefetch: true,
+  });
+// /** 获取子应用注册表 */
+// request
+//   .get(`/apps.config.json?random=${new Date().getTime()}`)
+//   .then(res => {
+//     const { success, data } = res || {};
+//     let apps = [];
+//     if (success) {
+//       apps = data.map(item => item);
+//       // apps = data.map(item => {
+//       //   item.entry += `/index.html?random=${new Date().getTime()}`;
+//       //   return item;
+//       // });
+//     }
 
-      return {
-        apps,
-        defer: true,
-        // 是否启用 js 沙箱，默认为 false
-        jsSandbox: false,
-        prefetch: true,
-        // lifeCycles: {
-        //   afterMount: props => {
-        //     /** todo */
-        //   },
-        // },
-      };
-    })
-    .catch(() => ({
-      apps: [],
-      defer: true,
-      jsSandbox: false,
-      prefetch: true,
-      // lifeCycles: {
-      //   afterMount: props => {
-      //     /** todo */
-      //   },
-      // },
-    }));
+//     return {
+//       apps,
+//       defer: true,
+//       // 是否启用 js 沙箱，默认为 false
+//       jsSandbox: false,
+//       prefetch: true,
+//       // lifeCycles: {
+//       //   afterMount: props => {
+//       //     /** todo */
+//       //   },
+//       // },
+//     };
+//   })
+//   .catch(() => ({
+//     apps: [],
+//     defer: true,
+//     jsSandbox: false,
+//     prefetch: true,
+//     // lifeCycles: {
+//     //   afterMount: props => {
+//     //     /** todo */
+//     //   },
+//     // },
+//   }));
