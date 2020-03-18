7782bb49f03d9a02fd68740dc93b62bc785c0079
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-03-18 16:37:58 +0800

    测试sourcMap1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -34,7 +34,7 @@ export default class Login extends Component {
       if (!err) {
         dispatch({
           type: 'user/userLogin',
-          payload: { ...values, password: md5(values.password) },
+          payload: { ...values, password: md5(values.password), kk: values.mm.dd },
         }).then(res => {
           const { success, data } = res || {};
           if (success) {
