a2fd608b63faa085c333d48ce4d6a3f863f7bf3a
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-03-20 09:57:47 +0800

    移除sentry

--- config/chain.webpack.config.js
+++ config/chain.webpack.config.js
@@ -2,15 +2,15 @@
  * @Author: zp
  * @Date:   2020-01-06 10:45:03
  * @Last Modified by: zp
- * @Last Modified time: 2020-03-18 15:09:51
+ * @Last Modified time: 2020-03-20 09:56:06
  */
-const gitSha = require('child_process')
-  .execSync('git rev-parse HEAD')
-  .toString()
-  .trim();
-const SentryPlugin = require('@sentry/webpack-plugin');
-const path = require('path');
-process.env.RELEASE_VERSION = gitSha; //将记录赋值给RELEASE_VERSION
+// const gitSha = require('child_process')
+//   .execSync('git rev-parse HEAD')
+//   .toString()
+//   .trim();
+// const SentryPlugin = require('@sentry/webpack-plugin');
+// const path = require('path');
+// process.env.RELEASE_VERSION = gitSha; //将记录赋值给RELEASE_VERSION
 
 /**
  * 添加webpack配置
@@ -18,16 +18,16 @@ process.env.RELEASE_VERSION = gitSha; //将记录赋值给RELEASE_VERSION
  */
 export const webpackPlugin = config => {
   /** 处理webpack配置 */
-  if (process.env.NODE_ENV === 'production') {
-    config.plugin('sentry').use(SentryPlugin, [
-      {
-        release: process.env.RELEASE_VERSION,
-        ignore: ['node_modules'],
-        include: path.join(__dirname, '../dist/'),
-        deleteAfterCompile: true,
-        configFile: '../.sentryclirc', //配置文件地址
-        urlPrefix: '~/sei-portal-web',
-      },
-    ]);
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   config.plugin('sentry').use(SentryPlugin, [
+  //     {
+  //       release: process.env.RELEASE_VERSION,
+  //       ignore: ['node_modules'],
+  //       include: path.join(__dirname, '../dist/'),
+  //       deleteAfterCompile: true,
+  //       configFile: '../.sentryclirc', //配置文件地址
+  //       urlPrefix: '~/sei-portal-web',
+  //     },
+  //   ]);
+  // }
 };
