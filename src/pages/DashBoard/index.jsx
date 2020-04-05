9678169b6730dc57fe868df7f9bfa3f5c294db06
eason <cheng.yi@capitalcrux.com>
2020-04-05 15:49:20 +0800

    [优化]

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.less';
 const  DashBoard=() => {
     return (
       <section className={styles['dashboard-wrapper']}>
-         <iframe src='/sei-dashboard-web/#/sei-dashboard-web/scene/kanban/p3g3wn'/>
+         <iframe src='/sei-dashboard-web/#/sei-dashboard-web/scene/kanban/76yxlo'/>
       </section>
     );
 }
