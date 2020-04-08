ad72527f9f76872084f4c85f3f03dd84ddaf2787
eason <cheng.yi@capitalcrux.com>
2020-04-08 22:20:49 +0800

    【平台首页】

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import styles from './index.less';
 
 const  DashBoard=() => {
-  const url=`/sei-dashboard-web/#/sei-dashboard-web/scene/kanban/76yxlo?rand=${Date.now()}`;
     return (
       <section className={styles['dashboard-wrapper']}>
-         <iframe src={url}/>
+         <iframe src="/sei-dashboard-web/#/sei-dashboard-web/scene/sei/home"/>
       </section>
     );
 }
