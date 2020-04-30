14d468f10ede6abba4d9bada3ac983fe637e3194
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-04-30 11:43:56 +0800

    修改样式引入地址

--- src/pages/Login/QrCode.jsx
+++ src/pages/Login/QrCode.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 class QrCode extends Component {
   componentDidMount() {
+    console.log(process.env);
     window.WwLogin({
       id: 'sei-qrcode',
       appid: 'wwdc99e9511ccac381',
       agentid: '1000003',
       redirect_uri: 'http%3A%2F%2Ftsei.changhong.com%3A8090%2Fapi-gateway%2Fsei-auth%2Fsso%2Flogin',
       state: '',
-      href: 'http://localhost:8000/qrcode.css',
+      href: `${window.host}/sei-portal-web/qrcode.css`,
     });
   }
 
