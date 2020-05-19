f784c69fa30d5e813978d5ffe5279beb4948e31f
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-05-19 15:54:22 +0800

    测试集成

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -139,39 +139,51 @@ export default class BasicLayout extends React.Component {
     getWeChatCfg().then(result => {
       const { success, data } = result || {};
       if (success) {
-        const { corpid, agentid, signature, nonceStr, timestamp } = data;
-        window.wx.agentConfig({
-          debug: true,
-          beta: true,
-          corpid,
-          agentid,
-          timestamp,
-          nonceStr,
-          signature,
-          jsApiList: ['openDefaultBrowser'],
-          success() {
-            const { sessionId: sid } = getCurrentUser() || {};
-            window.wx.invoke(
-              'openDefaultBrowser',
-              {
-                url: `${window.location.orgin}/sei-portal-web/#/sso/ssoWrapperPage?sid=${sid}`, // 在默认浏览器打开redirect_uri，并附加code参数；也可以直接指定要打开的url，此时不会附带上code参数。
-              },
-              res => {
-                // eslint-disable-next-line no-console
-                console.log('BasicLayout -> handleLogoClick -> res', res);
-                if (res.err_msg === 'openDefaultBrowser:ok') {
-                  window.wx.closeWindow();
-                  window.close();
-                }
-              },
-            );
-          },
-          fail(res) {
-            if (res.errMsg.indexOf('function not exist') > -1) {
-              message.warn('版本过低请升级');
-            }
-          },
+        const { corpid, agentid, signature, nonceStr, timestamp, agentSignature } = data;
+        window.wx.config({
+          beta: true, // 必须这么写，否则wx.invoke调用形式的jsapi会有问题
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: corpid, // 必填，企业微信的corpID
+          timestamp, // 必填，生成签名的时间戳
+          nonceStr, // 必填，生成签名的随机串
+          signature, // 必填，签名，见 附录-JS-SDK使用权限签名算法
+          jsApiList: ['openDefaultBrowser'], // 必填，需要使用的JS接口列表，凡是要调用的接口都需要传进来
         });
+        window.wx.ready(() => {
+          window.wx.agentConfig({
+            debug: true,
+            beta: true,
+            corpid,
+            agentid,
+            timestamp,
+            nonceStr: agentSignature,
+            signature,
+            jsApiList: ['openDefaultBrowser'],
+            success() {
+              const { sessionId: sid } = getCurrentUser() || {};
+              window.wx.invoke(
+                'openDefaultBrowser',
+                {
+                  url: `${window.location.orgin}/sei-portal-web/#/sso/ssoWrapperPage?sid=${sid}`, // 在默认浏览器打开redirect_uri，并附加code参数；也可以直接指定要打开的url，此时不会附带上code参数。
+                },
+                res => {
+                  // eslint-disable-next-line no-console
+                  console.log('BasicLayout -> handleLogoClick -> res', res);
+                  if (res.err_msg === 'openDefaultBrowser:ok') {
+                    window.wx.closeWindow();
+                    window.close();
+                  }
+                },
+              );
+            },
+            fail(res) {
+              if (res.errMsg.indexOf('function not exist') > -1) {
+                message.warn('版本过低请升级');
+              }
+            },
+          });
+        });
+
         // const { sessionId: sid } = getCurrentUser() || {};
         // window.wx.ready(() => {
         //   window.wx.invoke(
