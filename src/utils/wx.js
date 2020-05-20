1439a1421c2eb36b6872b1bffff74b2d26c5cc4a
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-05-20 10:47:09 +0800

    更新判断pc企业微信逻辑

--- src/utils/wx.js
+++ src/utils/wx.js
@@ -1,7 +1,7 @@
 /** 判断是否为pc端企业微信 */
 export const isWeiXin = () => {
   const ua = window.navigator.userAgent.toLowerCase();
-  if (ua.match(/windowswechat/i) === 'windowswechat' && ua.match(/wxwork/i) === 'wxwork') {
+  if (/wechat/.test(ua) && /wxwork/.test(ua)) {
     return true;
   }
   return false;
