6203bd02dae0369e60d4d369527ed422a4a40235
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-05-23 14:21:50 +0800

    修改dashboard

--- src/pages/DashBoard/index.jsx
+++ src/pages/DashBoard/index.jsx
@@ -7,7 +7,7 @@ const DashBoard = () => (
     <Iframe
       visible
       title="dashboard"
-      url="/sei-dashboard-web/#/sei-dashboard-web/scene/sei/home"
+      url="/sei-dashboard-web/scene/sei/home"
       id="portal-dashboard"
     />
   </section>
