bd83724cae4253ecb9d059996fbf2b039aaf4041
eason <cheng.yi@capitalcrux.com>
2020-07-01 22:40:44 +0800

    更改语言

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -1,12 +1,13 @@
 /*
  * @Author: zp
  * @Date:   2020-01-30 18:34:17
- * @Last Modified by:   zp
- * @Last Modified time: 2020-01-31 14:27:08
+ * @Last Modified by: Eason
+ * @Last Modified time: 2020-07-01 22:40:13
  */
 export default {
   'app.lang': 'Languages',
   'app.logout': 'Logout',
   'app.dashboard': 'DashBoard',
   'app.menusearch.placeholder': 'Please enter keywords to search',
+  'app.dashboard.custom': 'Custom Dashboard',
 };
