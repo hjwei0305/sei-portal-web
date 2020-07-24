4033c52c25ab220a5674697b07af17b62ecde713
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-07-24 15:46:53 +0800

    优化语音

--- src/utils/speechSynthesis.js
+++ src/utils/speechSynthesis.js
@@ -2,6 +2,7 @@ export default msg => {
   if (SpeechSynthesisUtterance) {
     const msgObj = new SpeechSynthesisUtterance(msg);
     Object.assign(msgObj, {
+      lang: 'zh-cn',
       volume: 100,
       rate: 1,
       pitch: 1.5,
