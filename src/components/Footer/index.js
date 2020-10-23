990b0d459df942f44987b61649b4e5dc7395eb38
eason <cheng.yi@capitalcrux.com>
2020-10-23 16:24:07 +0800

    UE

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,11 @@ const Footer = () => (
     style={{ position: 'absolute', bottom: 16, textAlign: 'center', width: '100%' }}
   >
     <div style={{ margin: '0 auto' }}>
-      四川虹信软件股份有限公司 <a href="http://www.rcsit.cn">www.rcsit.cn</a> | Copyright © 2020
+      四川虹信软件股份有限公司{' '}
+      <a href="http://www.rcsit.cn" style={{ color: '#999' }}>
+        www.rcsit.cn
+      </a>{' '}
+      | Copyright © 2020
     </div>
     <div style={{ margin: '0 auto' }}>
       地址：四川省成都市高新区天府四街199号长虹科技大厦A-27 电话：4000-870-780
