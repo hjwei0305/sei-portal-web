82066095fd9b7b81e8766c814a6c10b0a965a46b
Elric_H <yuxuan.he@changhong.com>
2022-01-04 22:46:44 +0800

    #南京项目临时改动004

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -112,7 +112,7 @@ export default class Login extends Component {
               <FormattedMessage id="app.title" defaultMessage="统一应用平台" />
             </div>
             <div className="desc">
-              <FormattedMessage id="app.subtitle" defaultMessage="智慧创造价值 专业赢得信赖" />
+              <FormattedMessage id="app.subtitle" defaultMessage="努力超越 追求卓越" />
             </div>
           </Animate>
         </div>
